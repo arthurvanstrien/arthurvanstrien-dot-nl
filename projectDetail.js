61bcc32b4699ffeb6765c393814489de2c3aef1f
Arthur van Strien <arthurvanstrien@gmail.com>
2019-09-18 00:25:15 +0200

    Refactored the JS from the project detail and added a func to display

--- projectDetail.js
+++ projectDetail.js
@@ -1,16 +1,53 @@
-function loadProjectdetailFunction(langFile) {
-	
-	var url = new URL(window.location.href);
-	
-	if(url.searchParams.get("additional")) {
-		var id = url.searchParams.get("additional");
-		getJSONFile("projects-list", displayProjectDetail, id, null);
-	}
-	else
-		displayErrorMessage(errorMessagesLanguageFile, "project-id-not-found");
-}
 
-function displayProjectDetail(langFile, projectId) {
+var projectDetail = (function() {
 	
+	var projDetail = {};
 	
-}
+	projDetail.load = function(langFile, project) { load(langFile, project); }
+	projDetail.getProjectFromListAndDisplay = function(langFile, projectId) { getProjectFromListAndDisplay(langFile, projectId); }
+
+	var load = function(langFile, project) {
+	
+		var url = new URL(window.location.href);
+		
+		if(project.projectId === 'undefined' || project.projectId === null) {
+			
+			//The previous page was not projects and therefore the projectData is NOT passed directly to this page.
+			//Instead we still have to load the list with projects and get the data for this project from that.
+			
+			if(url.searchParams.get("additional")) {
+				var id = url.searchParams.get("additional");
+				getJSONFile("projects-list", projectDetail.getProjectFromListAndDisplay, id, null, null);
+			}
+			else
+				displayErrorMessage(errorMessagesLanguageFile, "project-id-not-found");
+		}
+		else {
+			
+			displayProjectDetail(project);
+		}
+	}
+	
+	var getProjectFromListAndDisplay = function(langFile, projectId) {
+		
+		console.log("getProjectFromListAndDisplay");
+		
+		for(var i = 0; i < Object.keys(langFile.projects).length; i++) {
+			
+			var project = langFile.projects[i];
+			
+			if(project.projectId == projectId) {
+				
+				displayProjectDetail(project);
+			}
+		}
+	}
+	
+	var displayProjectDetail = function(project) {
+		
+		console.log("displayProjectDetail");
+	}
+	
+	return projDetail;
+	
+})();
