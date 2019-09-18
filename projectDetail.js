ae45807834c1ce0400a82c83639d6dcb89807c9f
Arthur van Strien <arthurvanstrien@gmail.com>
2019-09-18 16:40:17 +0200

    Created the overview of the project Detail page

--- projectDetail.js
+++ projectDetail.js
@@ -3,9 +3,11 @@ var projectDetail = (function() {
 	
 	var projDetail = {};
 	
+	
 	/*--ALL public methods in the projectDetail.js file----------------------------------------------------*/
 	projDetail.load = function(langFile, project) { load(langFile, project); }
-	projDetail.getProjectFromListAndDisplay = function(langFile, projectId) { getProjectFromListAndDisplay(langFile, projectId); }
+	//projDetail.getProjectFromListAndDisplay = function(langFile, projectId) { getProjectFromListAndDisplay(langFile, projectId); }
+
 
 	var load = function(langFile, project) {
 		
@@ -18,7 +20,7 @@ var projectDetail = (function() {
 			}
 			else {
 				
-				displayProjectDetail(project);
+				displayProjectDetail(langFile, project);
 			}
 		}
 		else {
@@ -37,7 +39,7 @@ var projectDetail = (function() {
 			
 			if(project.projectId == projectId) {
 				
-				displayProjectDetail(project);
+				displayProjectDetail(langFile, project);
 				found = true;
 			}
 		}
@@ -57,15 +59,40 @@ var projectDetail = (function() {
 		
 		if(url.searchParams.get("additional")) {
 			var id = url.searchParams.get("additional");
-			common.getJSONFile("projectsList", projectDetail.getProjectFromListAndDisplay, id, null, null);
+			common.getJSONFile("projectsList", common.displayContent, id, getProjectFromListAndDisplay, id);
 		}
 		else
 			common.displayErrorMessage(errorMessagesLanguageFile, "project-id-not-found");
 	}
 	
-	var displayProjectDetail = function(project) {
+	var displayProjectDetail = function(langFile, project) {
 		
 		var generatedHTML = "";
+		var textContent = project.text;
+		
+		console.log("displayProjectDetail");
+		
+		$("#lang_pageTitle").text(common.getFieldLanguage(project.title));
+		$("#projectDetail-year").text(project.year);
+		$("#projectDetail-category").text(common.getFieldLanguage(project.category));
+		$("#projectDetail-programming-languages").text(project.programmingLanguages);
+		$("#projectDetail-used-tools").text(project.usedTools);
+		$("#projectDetail-motive").text(common.getFieldLanguage(project.motive));
+		$("#projectDetail-version-control").text(common.getFieldLanguage(project.versionControl));
+		$("#projectDetail-process-framework").text(common.getFieldLanguage(project.processFramework));
+		$("#projectDetail-built-by").text(common.getFieldLanguage(project.builtBy));
+		
+		
+		for(var i = 0; i < Object.keys(textContent).length; i++) {
+		
+			generatedHTML = generatedHTML + common.getHTMLElement(textContent[i], i);
+		}
+		
+		$(generatedHTML).insertAfter("#project-text-anker");
+		
+		//Display the varlues from the default fields from the JSON file in the HTML.
+		//THe displayContent function already does that so we dont have to build a custom function here.
+		common.displayContent(langFile);
 	}
 	
 	return projDetail;
