126ea145da010a0b254972826ba2339f304a01a5
Arthur van Strien <arthurvanstrien@gmail.com>
2019-09-23 16:46:07 +0200

    The projects page changes the language of custom JSON data too.

--- projects.js
+++ projects.js
@@ -9,9 +9,13 @@ var projects = (function() {
 	/*--ALL public methods in the projects.js file----------------------------------------------------*/
 	projectsMethods.load = function(langFile) { load(langFile); }
 	projectsMethods.gotoProjectDetail = function(projectCounter) { gotoProjectDetail(projectCounter); }
+	projectsMethods.changeLanguage = function(langFile) { changeLanguage(langFile); }
 
 
 	var load = function(langFile) {
+		
+		//Set the additional language file so the language can be changed.
+		common.setAdditionalLanguageFile(langFile);
 		
 		projectsList = langFile.projects;
 		
@@ -19,17 +23,14 @@ var projects = (function() {
 			
 			var html = "";
 			var project = langFile.projects[i];
-			
-			//Add "" around the projectId and page so Javascript will interpret the value as a string instead as a variable.
-			var page = '"' + "projectDetail" + '"';
-			var projectId = '"' + project.projectId + '"'; 
+			var categoryId = "lang_categoryId_" + i;
 			
 			//Build the HTML for a project menu and fill it with JSON.
 			html = html + 
 			"<span onClick='projects.gotoProjectDetail(" + i + ")' class='projectsMenu-container'>" + 
 			"<h2 id='lang_project-title'>" + common.getFieldLanguage(project.title) + "</h2>" +
 			"<span class='projectsMenu-th'><span class='projectsMenu-td' id='lang_projectsMenu-year'>Year: </span><span>" + project.year + "</span></span>" +
-			"<span class='projectsMenu-th'><span class='projectsMenu-td' id='lang_projectsMenu-category'>Type: </span><span>" + common.getFieldLanguage(project.category) + "</span></span>";
+			"<span class='projectsMenu-th'><span class='projectsMenu-td' id='lang_projectsMenu-category'>Type: </span><span id=" + categoryId + ">" + common.getFieldLanguage(project.category) + "</span></span>";
 
 			var numPictures = Object.keys(project.pictures).length;
 			
@@ -56,6 +57,21 @@ var projects = (function() {
 	var gotoProjectDetail = function(projectCounter) {
 		
 		common.changePage("projectDetail", projectsList[projectCounter].projectId, projectsList[projectCounter]);
+	}
+	
+	var changeLanguage = function(langFile) {
+		
+		var projects = langFile.project;
+		
+		for(var i = 0; i < Object.keys(langFile.projects).length; i++) {
+			
+			var project = langFile.projects[i];
+			var categoryId = "#lang_categoryId_" + i;
+			
+			console.log(project.category);
+			
+			$(categoryId).text(common.getFieldLanguage(project.category));
+		}
 	}
 	
 	return projectsMethods;
