156fd0485e8985bce797cbb0625d44e5b979c2a0
Arthur van Strien <arthurvanstrien@gmail.com>
2019-10-25 15:11:43 +0200

    Added an engineer Tech page that displays a list of articles.

--- engineerTech.js
+++ engineerTech.js
@@ -1,1 +1,16 @@
-/*empty file */
+
+var engineerTech = (function() {
+	
+	var engineerTechMethods = {};
+	
+	/*---Engineer Tech Public function---------------------------------------------------------------------------------------------------------------*/
+	engineerTechMethods.changeLanguage = function() { changeLanguage(); }
+	
+	
+	var changeLanguage = function(langFile) { 
+	
+		common.displayContent(langFile);
+	}
+	
+	return engineerTechMethods;
+})();
